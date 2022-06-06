1313c2e7d518140009ce1db577783bca8cb6e3f1
Lucas Vieira <vieiralucas4@gmail.com>
2022-06-06 03:18:11 -0300

    feat: add Join to Future

--- future/future.go
+++ future/future.go
@@ -67,3 +67,104 @@ func All[A any](fs []*Future[A]) *Future[[]A] {
 		return as
 	})
 }
+
+func Join[A any, B any](fa *Future[A], fb *Future[B]) (A, B) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	var a A
+	var b B
+
+	go func() {
+		a = fa.Await()
+		defer wg.Done()
+	}()
+	go func() {
+		b = fb.Await()
+		defer wg.Done()
+	}()
+	wg.Wait()
+
+	return a, b
+}
+
+func Join3[A any, B any, C any](
+	fa *Future[A],
+	fb *Future[B],
+	fc *Future[C],
+) (A, B, C) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	var a A
+	var b B
+	var c C
+
+	go func() {
+		a, b = Join(fa, fb)
+		defer wg.Done()
+	}()
+	go func() {
+		c = fc.Await()
+		defer wg.Done()
+	}()
+	wg.Wait()
+
+	return a, b, c
+}
+
+func Join4[A any, B any, C any, D any](
+	fa *Future[A],
+	fb *Future[B],
+	fc *Future[C],
+	fd *Future[D],
+) (A, B, C, D) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	var a A
+	var b B
+	var c C
+	var d D
+
+	go func() {
+		a, b = Join(fa, fb)
+		defer wg.Done()
+	}()
+	go func() {
+		c, d = Join(fc, fd)
+		defer wg.Done()
+	}()
+	wg.Wait()
+
+	return a, b, c, d
+}
+
+func Join5[A any, B any, C any, D any, E any](
+	fa *Future[A],
+	fb *Future[B],
+	fc *Future[C],
+	fd *Future[D],
+	fe *Future[E],
+) (A, B, C, D, E) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	var a A
+	var b B
+	var c C
+	var d D
+	var e E
+
+	go func() {
+		a, b, c = Join3(fa, fb, fc)
+		defer wg.Done()
+	}()
+	go func() {
+		d, e = Join(fd, fe)
+		defer wg.Done()
+	}()
+	wg.Wait()
+
+	return a, b, c, d, e
+}
